02d810ba9dc9bf56b457411638abd7877261f293
tuhinchakraborty2009-lab <tuhin.chakraborty2009@gmail.com>
2026-04-03 13:46:47 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const WORK = [
+  {
+    title: "The Princess Diaries 3",
+    image: "images/work1.jpg",
+    description: "Leveraging cultural trends to drive engagement"
+  },
+  {
+    title: "Labubu vs Lafufu",
+    image: "images/work2.jpg",
+    description: "Expert guide bridging trends and authenticity"
+  }
+];
