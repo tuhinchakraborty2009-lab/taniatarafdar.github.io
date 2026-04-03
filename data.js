1c936ddf4c42d651644c5ce6a790f630a0b53680
tuhinchakraborty2009-lab <tuhin.chakraborty2009@gmail.com>
2026-04-03 16:30:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const WORK = [
-  {
-    title: "Under Fjords",
-    link: "https://www.travelandleisureasia.com/sea/trips/cruises/everything-about-fjord-cruise-from-bergen-norway/",
-    description: "Leveraging cultural trends to drive deep engagement"
-  },
-  {
-    title: "Labubu vs Lafufu",
-    link: "https://www.prestigeonline.com/hk/lifestyle/culture-plus-entertainment/how-to-spot-the-diffrence-between-labubu-and-lafufu/",
-    description: "Expert guide bridging viral trends and consumer insight"
-  }
+{
+title: "Princess Diaries 3",
+link: "https://example.com/article1",
+description: "Leveraging cultural trends to drive deep engagement"
+},
+{
+title: "Labubu vs Lafufu",
+link: "https://example.com/article2",
+description: "Expert guide bridging viral trends and consumer insight"
+}
 ];
